2767ec560e6bf4f6bcea4ae42715ec4ccd68cd7d
prabirshrestha <mail@prabir.me>
2012-08-10 15:21:42 -0700

    owin asp.net host

---
+++ src/SimpleOwinAspNetHost/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SimpleOwinAspNetHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.Add(new Route("{*pathInfo}", new SimpleOwinAspNetRouteHandler(new Helloworld().OwinApp)));
+        }
+    }
+}
