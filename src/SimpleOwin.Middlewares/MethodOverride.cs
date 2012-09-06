b4ff4d03df0ef59eb7624b1471c2f3ac65d5b208
prabirshrestha <mail@prabir.me>
2012-09-06 15:48:04 -0400

    methodOverride middleware checks for simpleOwin.body

--- src/SimpleOwin.Middlewares/MethodOverride.cs
+++ src/SimpleOwin.Middlewares/MethodOverride.cs
@@ -8,7 +8,7 @@
 
     public class MethodOverride
     {
-        public static Func<AppFunc, AppFunc> Middleware(string key = "_method")
+        public static Func<AppFunc, AppFunc> Middleware(string key = "_method", bool checkQuerystring = false, bool checkBody = true)
         {
             return
                 next =>
@@ -20,10 +20,40 @@
 
                     if (string.IsNullOrWhiteSpace(method))
                     {
+                        // try checking body, requires JsonBodyParser middleware
+                        if (checkBody)
+                        {
+                            var body = env.GetOwinEnvironmentValue<Lazy<object>>("simpleOwin.body");
+                            if (body != null)
+                            {
+                                try
+                                {
+                                    var dict = body.Value as IDictionary<string, object>;
+                                    if (dict != null)
+                                    {
+                                        object tempValue;
+                                        if (dict.TryGetValue(key, out tempValue))
+                                        {
+                                            var stringTempValue = tempValue as string;
+                                            if (!string.IsNullOrWhiteSpace(stringTempValue))
+                                                method = stringTempValue;
+                                        }
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    // do nothing, parsing json failed
+                                }
+                            }
+                        }
+
                         // try checking querystring, requires QueryParser middleware
-                        var qs = env.GetOwinEnvironmentValue<Lazy<IDictionary<string, string[]>>>("simpleOwin.query");
-                        if (qs != null)
-                            method = qs.Value.GetOwinHeaderValue(key);
+                        if (checkQuerystring && string.IsNullOrWhiteSpace(method))
+                        {
+                            var qs = env.GetOwinEnvironmentValue<Lazy<IDictionary<string, string[]>>>("simpleOwin.query");
+                            if (qs != null)
+                                method = qs.Value.GetOwinHeaderValue(key);
+                        }
                     }
 
                     if (!string.IsNullOrWhiteSpace(method))
