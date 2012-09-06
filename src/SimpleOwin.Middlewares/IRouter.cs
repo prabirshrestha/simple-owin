244e52068ab529c934a7df61ade3a85261a40453
prabirshrestha <mail@prabir.me>
2012-09-06 19:29:07 -0400

    IRouter

---
+++ src/SimpleOwin.Middlewares/IRouter.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleOwin.Middlewares
+{
+    using System;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    public interface IRouter
+    {
+        void All(Func<AppFunc, AppFunc> callback);
+        void All(string route, Func<AppFunc, AppFunc> callback);
+
+        void Get(string route, Func<AppFunc, AppFunc> callback);
+        void Post(string route, Func<AppFunc, AppFunc> callback);
+        void Put(string route, Func<AppFunc, AppFunc> callback);
+        void Delete(string route, Func<AppFunc, AppFunc> callback);
+        void Head(string route, Func<AppFunc, AppFunc> callback);
+        void Options(string route, Func<AppFunc, AppFunc> callback);
+        void Patch(string route, Func<AppFunc, AppFunc> callback);
+
+        Func<AppFunc, AppFunc> Middleware();
+    }
+}
