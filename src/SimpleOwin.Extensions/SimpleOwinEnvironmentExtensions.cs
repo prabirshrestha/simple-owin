45a730c01b6c337b9b7c1e84c34b63a72e987a0f
prabirshrestha <mail@prabir.me>
2012-09-06 12:14:08 -0400

    GetOwinResponseHeaders Action<headers> callback

--- src/SimpleOwin.Extensions/SimpleOwinEnvironmentExtensions.cs
+++ src/SimpleOwin.Extensions/SimpleOwinEnvironmentExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace SimpleOwin.Extensions
 {
+    using System;
     using System.Collections.Generic;
 
     public static class SimpleOwinEnvironmentExtensions
@@ -7,6 +8,14 @@
         public static IDictionary<string, string[]> GetOwinResponseHeaders(this IDictionary<string, object> env)
         {
             return env.GetOwinEnvironmentValue<IDictionary<string, string[]>>("owin.ResponseHeaders");
+        }
+
+        public static IDictionary<string, object> GetOwinResponseHeaders(this IDictionary<string, object> env, Action<IDictionary<string, string[]>> callback)
+        {
+            var headers = env.GetOwinResponseHeaders();
+            if (callback != null)
+                callback(headers);
+            return env;
         }
     }
 }
