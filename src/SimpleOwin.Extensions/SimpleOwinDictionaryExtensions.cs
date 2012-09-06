95da3bac10b903b7a644ea9d714c3f7115e06b54
prabirshrestha <mail@prabir.me>
2012-09-06 12:11:29 -0400

    SimpleOwinDictionaryExtensions.GetOwinEnvironmentValue

---
+++ src/SimpleOwin.Extensions/SimpleOwinDictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleOwin.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class SimpleOwinDictionaryExtensions
+    {
+        public static T GetOwinEnvironmentValue<T>(this IDictionary<string, object> env, string key, T defaultValue = default(T))
+        {
+            object value;
+            return env.TryGetValue(key, out value) && value is T ? (T)value : defaultValue;
+        }
+    }
+}
