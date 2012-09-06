befeca5fdcb49bbcf448394c2b9f359212a04d22
prabirshrestha <mail@prabir.me>
2012-09-06 11:10:20 -0400

    GetOwinResponseBody extension

--- src/SimpleOwin.Middlewares/Extensions/SimpleOwinEnvironmentExtensions.cs
+++ src/SimpleOwin.Middlewares/Extensions/SimpleOwinEnvironmentExtensions.cs
@@ -1,12 +1,18 @@
 ﻿namespace SimpleOwin.Middlewares.Extensions
 {
     using System.Collections.Generic;
+    using System.IO;
 
     public static class SimpleOwinEnvironmentExtensions
     {
         public static IDictionary<string, string[]> GetOwinResponseHeaders(this IDictionary<string, object> env)
         {
             return env.GetSimpleOwinValue<IDictionary<string, string[]>>("owin.ResponseHeaders");
+        }
+
+        public static Stream GetOwinResponseBody(this IDictionary<string, object> env )
+        {
+            return env.GetSimpleOwinValue<Stream>("owin.ResponseBody");
         }
 
         public static T GetSimpleOwinValue<T>(this IDictionary<string, object> env, string key, T defaultValue = default(T))
