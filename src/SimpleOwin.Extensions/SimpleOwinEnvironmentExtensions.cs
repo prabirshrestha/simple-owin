f37f28b99dc4bcc96c33ac879f3e21f609d792b4
prabirshrestha <mail@prabir.me>
2012-09-06 12:13:08 -0400

    SimpleOwinEnvironmentExtensions.GetOwinResponseHeaders

---
+++ src/SimpleOwin.Extensions/SimpleOwinEnvironmentExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleOwin.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class SimpleOwinEnvironmentExtensions
+    {
+        public static IDictionary<string, string[]> GetOwinResponseHeaders(this IDictionary<string, object> env)
+        {
+            return env.GetOwinEnvironmentValue<IDictionary<string, string[]>>("owin.ResponseHeaders");
+        }
+    }
+}
