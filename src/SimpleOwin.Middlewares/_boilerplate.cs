235eccc3ee15d3ae44b10bc496a204a2cc0bdda1
prabirshrestha <mail@prabir.me>
2012-09-06 10:51:53 -0400

    middleware boilerplate

---
+++ src/SimpleOwin.Middlewares/_boilerplate.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleOwin.Middlewares
+{
+    using System;
+
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    public class SimpleOwinMiddlewareBoilerplate
+    {
+         public static Func<AppFunc, AppFunc> Middleware()
+         {
+             return next =>
+                    env =>
+                        {
+                            return next(env);
+                        };
+         }
+    }
+}
