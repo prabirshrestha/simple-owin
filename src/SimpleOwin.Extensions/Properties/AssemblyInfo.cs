19759122af1a249397e2273f2b10d4463a8a2536
prabirshrestha <mail@prabir.me>
2012-09-12 11:53:54 -0400

    added Portable Class Library support

--- src/SimpleOwin.Extensions/Properties/AssemblyInfo.cs
+++ src/SimpleOwin.Extensions/Properties/AssemblyInfo.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fceffbd0-1b8a-4422-8f5c-0535bac1a02c")]
+//[assembly: Guid("fceffbd0-1b8a-4422-8f5c-0535bac1a02c")]
 
 // Version information for an assembly consists of the following four values:
 //
