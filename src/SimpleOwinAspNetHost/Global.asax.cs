845cbbdac6a3d043a7d3058513013041301e1fcb
prabirshrestha <mail@prabir.me>
2012-09-14 01:01:28 -0400

    SimpleOwinAspNetHost-Mono.csproj

--- src/SimpleOwinAspNetHost/Global.asax.cs
+++ src/SimpleOwinAspNetHost/Global.asax.cs
@@ -5,9 +5,10 @@ namespace SimpleOwinAspNetHost
     using System.Web.Routing;
     using SimpleOwin.Hosts.AspNet;
     using SimpleOwinAspNetHost.Samples;
+#if !MONO
     using SimpleOwinAspNetHost.Samples.WebSockets.Helloworld;
     using SimpleOwinAspNetHost.Samples.WebSockets.HelloworldAutodetect;
-
+#endif
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
@@ -19,10 +20,12 @@ namespace SimpleOwinAspNetHost
             // SimpleOwinAspNetRouteHandler is capable of auto handling IEnumerable<Func<AppFunc,AppFunc>>
             RouteTable.Routes.Add(new Route("middlewareapps", new SimpleOwinAspNetRouteHandler(MiddlewareApps.OwinApps())));
 
+#if !MONO
             RouteTable.Routes.Add(new Route("SimpleOwinApp/{*pathInfo}", new SimpleOwinAspNetRouteHandler(SimpleOwinApp.OwinApp(), "SimpleOwinApp")));
 
             RouteTable.Routes.Add(new Route("websocket/helloworld", new SimpleOwinAspNetRouteHandler(HelloWorldWebSocket.OwinApp())));
             RouteTable.Routes.Add(new Route("websocket/helloworld/autodetect", new SimpleOwinAspNetRouteHandler(HelloWorldWebSocketAutodetect.OwinApp())));
+#endif
         }
     }
 }
